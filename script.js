3b59c1f4cfed9f61f186d3eb6b0e168260a5c1c5
darnellprime <rosediditlol@gmail.com>
2026-05-20 12:35:46 -0400

    s

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-// Smooth navbar transition
+// NAVBAR EFFECT
 
 window.addEventListener("scroll", () => {
 
   const navbar = document.querySelector(".navbar");
 
   if(window.scrollY > 80){
+
     navbar.style.background = "rgba(0,0,0,0.85)";
     navbar.style.padding = "18px 8%";
+
   } else {
+
     navbar.style.background = "rgba(0,0,0,0.45)";
     navbar.style.padding = "25px 8%";
+
   }
 
 });
 
 
-// Fade-in animation on scroll
+// SCROLL ANIMATIONS
 
 const observer = new IntersectionObserver((entries) => {
 
   entries.forEach(entry => {
 
     if(entry.isIntersecting){
+
       entry.target.style.opacity = "1";
       entry.target.style.transform = "translateY(0px)";
+
     }
 
   });
 
-}, {
+},{
   threshold:0.15
 });
 
